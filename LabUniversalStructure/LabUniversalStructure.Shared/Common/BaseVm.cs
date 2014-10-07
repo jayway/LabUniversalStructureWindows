83e3a4952108f2e3cd639f49f010c568b6779560
RobertHedgate <robert.hedgate@jayway.com>
2014-10-07 14:05:13 +0200

    first commit

---
+++ LabUniversalStructure/LabUniversalStructure.Shared/Common/BaseVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LabUniversalStructure.Common
+{
+    class BaseVm : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ LabUniversalStructure/LabUniversalStructure.Windows/ViewModels/MainPageVm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using LabUniversalStructure.Common;
+
+namespace LabUniversalStructure.ViewModels
+{
+    class MainPageVm : BaseVm
+    {
+        public BitmapImage Image { get; set; }
+
+        public async void GetImage()
+        {
+            var openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary
+            };
+            openPicker.FileTypeFilter.Add(".jpg");
+            var file = await openPicker.PickSingleFileAsync();
+
+            SetImage(file);
+        }
+
+        public async void SetImage(StorageFile file)
+        {
+            try
+            {
+                var stream = await file.OpenReadAsync();
+                Image = new BitmapImage();
+                Image.SetSource(stream);
+            }
+            catch (Exception e)
+            {
+                // Handle error
+            }
+        }
+
+    }
+}
